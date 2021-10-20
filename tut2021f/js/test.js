b059981142bc1fc06e50cdca7b5d5d0a1d7393a7
Yuetatsu <m0119140ba@edu.teu.ac.jp>
2021-10-20 18:38:37 +0900

    change test2.html file

--- tut2021f/js/test.js
+++ tut2021f/js/test.js
@@ -1,3 +1,16 @@
 console.log("Hello World");
 console.log("Console Log Test");
 console.log("Welcome to JavaScript!");
+let hue = 0;
+
+const changeColor=function() {
+  let id = document.getElementById("cc");
+  id.style.color = "hsl(" + hue + ", 100%, 50%)";
+  hue++;
+  hue %= 360;
+  setTimeout(changeColor, 1000/60)
+}
+
+window.onload=function() {
+  changeColor();
+}
