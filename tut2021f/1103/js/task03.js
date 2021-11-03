b426f2425862555825a5af2f3e8a6e1d53225669
Yuetatsu <m0119140ba@edu.teu.ac.jp>
2021-11-03 18:55:09 +0900

    add some files

---
+++ tut2021f/1103/js/task03.js
@@ -0,0 +1,9 @@
+let modal = document.querySelector("#modal");
+
+function appendCircle() {
+  modal.innerHTML = modal.innerHTML + "〇";
+}
+
+function appendCross() {
+  modal.innerHTML = modal.innerHTML + "×";
+}
